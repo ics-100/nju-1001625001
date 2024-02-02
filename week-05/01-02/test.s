9c54c0ed6b9f4bc4b0622c98c0fa7875765a0434
guxiaobai <sikuan.gu@gmail.com>
2024-02-02 20:56:34 +0800

    misc

---
+++ week-05/01-02/test.s
@@ -0,0 +1,26 @@
+	.arch armv8-a
+	.file	"test.c"
+	.text
+	.align	2
+	.global	add
+	.type	add, %function
+add:
+.LFB0:
+	.cfi_startproc
+	sub	sp, sp, #32
+	.cfi_def_cfa_offset 32
+	str	w0, [sp, 12]
+	str	w1, [sp, 8]
+	ldr	w1, [sp, 12]
+	ldr	w0, [sp, 8]
+	add	w0, w1, w0
+	str	w0, [sp, 28]
+	ldr	w0, [sp, 28]
+	add	sp, sp, 32
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	add, .-add
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
